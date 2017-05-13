2f03b4abe6ee4add48e6025898a34f9759d51130
Raphael Guntersweiler <raphael@rgunti.ch>
2017-05-13 02:15:54 +0200

    Added GPS to project

---
+++ LocaTracker2.Gps/GpsUtilities.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocaTracker2.Gps
+{
+    /// <summary>
+    /// A collection of functions that are used to calculate
+    /// and convert GPS related data
+    /// </summary>
+    public static class GpsUtilities
+    {
+        /// <summary>
+        /// Contains conversion methods for Metrical and Imperial units
+        /// </summary>
+        public static class MetricImperialConverter
+        {
+            /// <summary>
+            /// Converts Distance in Meters [m] to Yards [yd]
+            /// </summary>
+            public static double ConvertMeterToYard(double meters) { return meters * 1.0936; }
+            /// <summary>
+            /// Converts Distance in Meters [m] to Feet [ft]
+            /// </summary>
+            public static double ConvertMeterToFeet(double meters) { return meters * 3.28084; }
+
+            /// <summary>
+            /// Converts Distance in Kilometers [km] to miles [mi]
+            /// </summary>
+            public static double ConvertKMtoMile(double km) { return km * 0.62137; }
+
+            /// <summary>
+            /// Converts Speed in Miles per Hour [mph] to Meters per Second [m/s]
+            /// </summary>
+            public static double ConvertMPHtoMPS(double mph) { return mph / 2.23694; }
+            /// <summary>
+            /// Converts Speed in Meters per Second [m/s] to Miles per Hour [mps]
+            /// </summary>
+            public static double ConvertMPStoMPH(double mps) { return mps * 2.23694; }
+        }
+
+        /// <summary>
+        /// Converts Speed in Kilometers per Hour [km/h] to Meters per Second [m/s]
+        /// </summary>
+        public static double ConvertKMHtoMPS(double kmh) { return kmh / 3.6; }
+        /// <summary>
+        /// Converts Speed in Meters per Second [m/s] to Kilometers per Hour [km/h]
+        /// </summary>
+        public static double ConvertMPStoKMH(double mps) { return mps * 3.6; }
+
+        /// <summary>
+        /// Calculates the distance between two given coordinates in meters
+        /// </summary>
+        /// <param name="latA">Point A: Latitude</param>
+        /// <param name="lonA">Point A: Longitude</param>
+        /// <param name="latB">Point B: Latitude</param>
+        /// <param name="lonB">Point B: Longitude</param>
+        /// <returns>Distance between Point A and B in meters</returns>
+        public static double GetDistanceBetweenTwoPoints(double latA, double lonA, double latB, double lonB)
+        {
+            double R = 6371 * 1000;
+            double dLat = ConvertToRad(latB - latA);
+            double dLon = ConvertToRad(lonB - lonA);
+            double a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+                Math.Cos(ConvertToRad(latA)) * Math.Cos(ConvertToRad(latB)) *
+                Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+            double c = 2 * Math.Asin(Math.Min(1, Math.Sqrt(a)));
+            double d = R * c;
+            return d;
+        }
+
+        private static double ConvertToRad(double val) { return (Math.PI / 180) * val; }
+    }
+}
