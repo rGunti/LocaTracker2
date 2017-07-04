06d76058dd7e8c525ad7708b50a50b9d9521b9f5
Raphael Guntersweiler <raphael@rgunti.ch>
2017-07-05 01:31:58 +0200

    Created an abstract GPS Tracker

--- LocaTracker2.Gps/GpsTracker.cs
+++ LocaTracker2.Gps/GpsTracker.cs
@@ -3,11 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
 
 namespace LocaTracker2.Gps
 {
-    public class GpsTracker
+    public abstract class GpsTracker<T> where T : GpsTracker<T>, new()
     {
+        #region Singleton
+        protected static T instance;
+        public static T Instance {
+            get {
+                if (instance == null) instance = new T();
+                return instance;
+            }
+        }
+        #endregion Singleton
 
+        protected Geolocator locator = null;
+        protected GeolocationAccessStatus accessStatus;
+        protected PositionStatus positionStatus;
+
+        protected bool settingUp = false;
+
+        #region Setup
+        protected async Task InitAsync()
+        {
+            settingUp = true;
+
+            accessStatus = await Geolocator.RequestAccessAsync();
+            switch (accessStatus)
+            {
+                case GeolocationAccessStatus.Allowed:
+                    locator = new Geolocator()
+                    {
+                        ReportInterval = 500,
+                        DesiredAccuracy = PositionAccuracy.High
+                    };
+                    locator.PositionChanged += Locator_PositionChanged;
+                    locator.StatusChanged += Locator_StatusChanged;
+                    break;
+                case GeolocationAccessStatus.Denied:
+                case GeolocationAccessStatus.Unspecified:
+                default:
+                    locator = null;
+                    break;
+            }
+
+            settingUp = false;
+        }
+        #endregion Setup
+
+        #region Event Status
+        protected abstract void Locator_StatusChanged(Geolocator sender, StatusChangedEventArgs args);
+        protected abstract void Locator_PositionChanged(Geolocator sender, PositionChangedEventArgs args);
+        #endregion Event Status
     }
 }
