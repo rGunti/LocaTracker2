56dd4e85e1e943622bf2b262e480495a7fd298ec
Raphael Guntersweiler <raphael@rgunti.ch>
2017-07-05 10:50:58 +0200

    Added Missing Permission Dialog (still unused)

---
+++ LocaTracker2/Views/Dialogs/MissingPermissionDialog.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Content Dialog item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace LocaTracker2.Views.Dialogs
+{
+    public sealed partial class MissingPermissionDialog : ContentDialog
+    {
+        public MissingPermissionDialog()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        { }
+
+        private async void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            await Launcher.LaunchUriAsync(new Uri("ms-settings:privacy-location"));
+        }
+    }
+}
