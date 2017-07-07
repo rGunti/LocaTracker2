b4b0ff6e030f34dd4ed52ad05edde049b83cb995
Raphael Guntersweiler <raphael@rgunti.ch>
2017-07-08 00:36:50 +0200

    Implemented own logger (UWP is too complicated)

---
+++ LocaTracker2.Logging/BaseLogger.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocaTracker2.Logging
+{
+    public enum LogLevel
+    {
+        Verbose,
+        Debug,
+        Info,
+        Warning,
+        Error,
+        Failure,
+        Always
+    }
+
+    public abstract class BaseLogger<T> where T : BaseLogger<T>, new()
+    {
+        protected static T instance;
+        public static T Instance {
+            get {
+                if (instance == null) instance = new T();
+                return instance;
+            }
+        }
+
+        protected BaseLogger() { InitializeLogger(); }
+        
+        public bool UseUtcTimes { get; set; } = true;
+
+        protected virtual void InitializeLogger() { }
+
+        protected abstract void ProcessEntry(string entry);
+
+        protected virtual string ComposeMessage(LogLevel level, Type source, string message) =>
+            $"{(UseUtcTimes ? DateTime.UtcNow : DateTime.Now):yyyy-MM-dd HH:mm:ss.ffff zzz}|{level,-7}|{source.Name}|{message}";
+
+        public void Log(LogLevel level, object source, string message) => 
+            ProcessEntry(ComposeMessage(level, source.GetType(), message));
+        public void Log(LogLevel level, Type sourceType, string message) =>
+            ProcessEntry(ComposeMessage(level, sourceType, message));
+
+        public void V(object source, string message) => Log(LogLevel.Verbose, source, message);
+        public void V(Type sourceType, string message) => Log(LogLevel.Verbose, sourceType, message);
+
+        public void D(object source, string message) => Log(LogLevel.Debug, source, message);
+        public void D(Type sourceType, string message) => Log(LogLevel.Debug, sourceType, message);
+
+        public void I(object source, string message) => Log(LogLevel.Info, source, message);
+        public void I(Type sourceType, string message) => Log(LogLevel.Info, sourceType, message);
+
+        public void W(object source, string message) => Log(LogLevel.Warning, source, message);
+        public void W(Type sourceType, string message) => Log(LogLevel.Warning, sourceType, message);
+
+        public void E(object source, string message) => Log(LogLevel.Error, source, message);
+        public void E(Type sourceType, string message) => Log(LogLevel.Error, sourceType, message);
+
+        public void A(object source, string message) => Log(LogLevel.Always, source, message);
+        public void A(Type sourceType, string message) => Log(LogLevel.Always, sourceType, message);
+    }
+}
---
+++ LocaTracker2.Logging/StorageFileLogger.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace LocaTracker2.Logging
+{
+    public class StorageFileLogger : BaseLogger<StorageFileLogger>
+    {
+        private Queue<string> messageQueue = new Queue<string>();
+        private StorageFile storageFile;
+        private bool isRunning = false;
+
+        protected override async void InitializeLogger()
+        {
+            storageFile = await ApplicationData.Current.LocalFolder.CreateFileAsync(
+                $"LocaTracker2_{DateTime.UtcNow:yyyyMMdd_HHmmss}Z.log", 
+                CreationCollisionOption.OpenIfExists
+            );
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            Task.Run(async () => {
+                I(this, $"Background Thread has been written...");
+                isRunning = true;
+
+                while (isRunning) {
+                    List<string> lines = new List<string>();
+                    while (messageQueue.Count > 0) {
+                        WriteToFile(messageQueue.Dequeue());
+                    }
+                    await Task.Delay(1000);
+                }
+            });
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+        }
+
+        private async void WriteToFile(string line)
+        {
+            try {
+                await FileIO.AppendTextAsync(storageFile, line + Environment.NewLine);
+            } catch { }
+        }
+
+        protected override void ProcessEntry(string entry) => messageQueue.Enqueue(entry);
+
+        public void Shutdown() { isRunning = false; }
+    }
+}
